9073d27d188b40cf566b3faeb60be2c722427079
formesean <seanaguilar698@gmail.com>
2025-06-25 12:50:46 +0800

    wip (3)

--- src/LAB/LAB_Software_Navigation.h
+++ src/LAB/LAB_Software_Navigation.h
@@ -2,7 +2,6 @@
 #define LAB_SOFTWARE_NAVIGATION_H
 
 #include "LAB_Module.h"
-#include "../Utility/LAB_Definitions.h"
 
 #include <iostream>
 #include <iomanip>
@@ -10,10 +9,10 @@
 class LAB_Software_Navigation : public LAB_Module
 {
   private:
-    LAB_Parent_Data_Software_Navigation m_parent_data;
+    LAB_Software_Navigation m_parent_data;
 
   private:
-    void parse_and_handle_packet (uint16_t* packet)
+    void parse_and_handle_packet (uint16_t* packet);
 
   public:
     LAB_Software_Navigation (LAB& LAB);
