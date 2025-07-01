8deab7845b8a48031bbe6bffecfee541fafa9b3f
formesean <seanaguilar698@gmail.com>
2025-07-01 16:31:42 +0800

    wip (14)

--- src/LABSoft_Presenter/LABSoft_Presenter_Software_Navigation.cpp
+++ src/LABSoft_Presenter/LABSoft_Presenter_Software_Navigation.cpp
@@ -17,13 +17,31 @@ LABSoft_Presenter_Software_Navigation::
 update_data_cycle()
 {
   auto data = lab().m_Software_Navigation.update_spi_data();
+
   if (data[0] == 0 && data[1] == 0 && data[2] == 0)
     return;
 
   printf("Type: %u, Action: %u, Value: %u\n", data[0], data[1], data[2]);
 
   if (data[1] == 3 && data[2] == 0)
-    lab().m_Oscilloscope.run();
+  {
+    int state = gui().oscilloscope_fl_light_button_run_stop->value();
+    printf("osc run btn state: %d\n", !state);
+
+    if (!state)
+    {
+      presenter ().m_Oscilloscope.stop_gui ();
+      presenter ().m_Voltmeter   .stop_gui ();
+      presenter ().m_Ohmmeter    .stop_gui ();
+      lab ().m_Oscilloscope.run ();
+      presenter ().m_Oscilloscope.run_gui ();
+    }
+    else
+    {
+      lab ().m_Oscilloscope.stop ();
+      presenter ().m_Oscilloscope.stop_gui ();
+    }
+  }
 }
 
 // EOF
