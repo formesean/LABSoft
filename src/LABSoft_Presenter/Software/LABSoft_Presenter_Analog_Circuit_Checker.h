6f914f47d57950a1625e6288b9bb3ae7aaa6f7b8
vaebelle <beabelle.canos@gmail.com>
2025-08-09 15:57:49 +0800

    wip: capture data on button click

--- src/LABSoft_Presenter/Software/LABSoft_Presenter_Analog_Circuit_Checker.h
+++ src/LABSoft_Presenter/Software/LABSoft_Presenter_Analog_Circuit_Checker.h
@@ -3,17 +3,22 @@
 #define LABSOFT_PRESENTER_ANALOG_CIRCUIT_CHECKER_H
 
 #include "../LABSoft_Presenter_Unit.h"
+#include <FL/Fl_Button.H>
 
 
 class LABSoft_Presenter_Analog_Circuit_Checker : public LABSoft_Presenter_Unit
 {
   private:
-    void load_gui ();
+    void load_gui                                           ();
+    bool can_capture_signal                                 () const;
+    void capture_oscilloscope_and_function_generator_data   ();
 
   public:
     LABSoft_Presenter_Analog_Circuit_Checker (LABSoft_Presenter& _LABSoft_Presenter);
     
-    void update_display       ();
+    void update_display             ();
+    void init_gui_callbacks         ();
+    static void cb_capture_signal   (Fl_Button* w, void* data);
 };
 
 #endif
