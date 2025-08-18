3cbc4292759f62183f43405eefbd6fe1b52d82a3
Von Juztis A. Elciario <vjelciario@gmail.com>
2025-08-18 11:13:38 +0800

    Update LAB_Oscilloscope.cpp

--- src/LAB/LAB_Oscilloscope.cpp
+++ src/LAB/LAB_Oscilloscope.cpp
@@ -595,6 +595,27 @@ fill_raw_osc_samp_buff_from_dma_buff ()
     static_cast<LAB_DMA_Data_Oscilloscope*>(m_uncached_memory.virt ())
   );
 
+  unsigned sz = m_parent_data.raw_data_buffer.size();
+  double amplitude = 1500;
+  double offset    = 2048;
+  double freq_hz   = 1000; // 1kHz sine
+  double sample_rate = m_parent_data.sampling_rate;
+
+for (unsigned i = 0; i < sz; ++i)
+{
+  double t = static_cast<double>(i) / sample_rate;
+  double sine_val = amplitude * sin(2.0 * M_PI * freq_hz * t);
+  int val = static_cast<int>(offset + sine_val);
+
+  if (val < 0) val = 0;
+  else if (val > 4095) val = 4095;
+
+  uint16_t ch1 = static_cast<uint16_t>(val);
+  uint16_t ch2 = ch1;
+
+  m_parent_data.raw_data_buffer[i] = (ch2 << 16) | ch1;
+}
+
   switch (m_parent_data.mode)
   {
     case (LABE::OSC::MODE::REPEATED):
@@ -1985,5 +2006,6 @@ calibration ()
 {
   return (m_calibration);
 }
+
 
 // EOF
