0bfcbec79bd75b2b6b6d24c7dafb24c4ccee1378
Von Juztis A. Elciario <vjelciario@gmail.com>
2025-08-21 22:52:13 +0800

    Update LABSoft_Presenter_LABChecker_Analog.cpp

--- src/LABSoft_Presenter/Software/LABSoft_Presenter_LABChecker_Analog.cpp
+++ src/LABSoft_Presenter/Software/LABSoft_Presenter_LABChecker_Analog.cpp
@@ -159,6 +159,12 @@ update_gui_with_captured_data()
   LAB_Oscilloscope_Display& osc_disp = lab().m_Oscilloscope_Display;
   osc.update_data_samples();
 
+   auto& raw_buf = osc.parent_data().raw_data_buffer;
+    std::cout << "[DEBUG] Raw buffer first 10: ";
+    for (size_t i = 0; i < std::min<size_t>(10, raw_buf.size()); i++)
+        std::cout << raw_buf[i] << " ";
+    std::cout << std::endl
+
   LABSoft_GUI_LABChecker_Analog_Checker_Display& analog_checker_disp_gui =
     *(gui ().analog_labsoft_gui_analog_checker_display);
 
